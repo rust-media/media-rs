9c690d853ef13d9c207f6141e061b866c5b59d61
Zhou Wei <zhouwei@ehom.net>
2025-09-12 21:09:25 +0800

    fix(media-device): adjust module loading for target OS compatibility

--- media-device/src/lib.rs
+++ media-device/src/lib.rs
@@ -1,4 +1,5 @@
 #[cfg(feature = "video")]
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "ios"))]
 pub mod camera;
 
 mod device;
@@ -7,16 +8,11 @@ use cfg_if::cfg_if;
 pub use device::*;
 
 cfg_if! {
-    if #[cfg(any(target_os = "ios", target_os = "macos"))] {
-        #[path = "mac/mod.rs"]
-        pub mod backend;
-    } else if #[cfg(target_os = "windows")] {
+    if #[cfg(target_os = "windows")] {
         #[path = "windows/mod.rs"]
         pub mod backend;
-    } else if #[cfg(target_family = "wasm")] {
-        #[path = "web/mod.rs"]
+    } else if #[cfg(any(target_os = "macos", target_os = "ios"))] {
+        #[path = "mac/mod.rs"]
         pub mod backend;
-    } else {
-        compile_error!("unsupported target");
     }
 }
