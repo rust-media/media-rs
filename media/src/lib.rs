31396af3f65e10215c9b7d5346500434fcf4853d
Zhou Wei <zhouwei@ehom.net>
2026-02-23 21:53:29 +0800

    fix(media): add media_format export

--- media/src/lib.rs
+++ media/src/lib.rs
@@ -3,3 +3,5 @@ pub use media_codec as codec;
 pub use media_core::{media::*, *};
 #[cfg(feature = "device")]
 pub use media_device as device;
+#[cfg(feature = "format")]
+pub use media_format as format;
